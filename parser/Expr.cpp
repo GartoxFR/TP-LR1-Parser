fb14b09f2fcc41231916580cc5b7d29b792c90e9
Gartox <ewan71230@gmail.com>
2024-02-21 09:22:39 +0100

    Mul expr

--- parser/Expr.cpp
+++ parser/Expr.cpp
@@ -22,3 +22,15 @@ uint32_t Add::eval(){
     return (this->leftExpr->eval()+this->rightExpr->eval());
 }
 
+std::ostream &Mul::print(std::ostream &o) {
+    o << "(";
+    this->leftExpr->print(o);
+    o << " * ", this->rightExpr->print(o);
+    this->rightExpr->print(o);
+    o << ")";
+    return o;
+}
+
+uint32_t Mul::eval() {
+    return this->leftExpr->eval() * this->rightExpr->eval();
+} 

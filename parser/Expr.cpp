f91c1046041c82fc146ce0fdda3a7afb575665ce
Toto <theo.chone@gmail.com>
2024-02-21 09:20:51 +0100

    Add et Val

--- parser/Expr.cpp
+++ parser/Expr.cpp
@@ -1,3 +1,22 @@
 #include "parser/Expr.h"
 
+std::ostream &Val::print(std::ostream &o){
+    o << this->value;
+    return o;
+}
+
+uint32_t Val::eval(){
+    return this->value;
+}
+
+std::ostream &Add::print(std::ostream &o){
+    this->leftExpr->print(o);
+    o << " + ";
+    this->rightExpr->print(o);
+    return o;
+}
+      
+uint32_t Add::eval(){
+    return (this->leftExpr->eval()+this->rightExpr->eval());
+}
 
